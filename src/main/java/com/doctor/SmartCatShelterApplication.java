451fee9053c4ba7ca01c205b2c1d14b5a991717a
李 <3411859729@qq.com>
2026-06-16 23:01:13 +0800

    Add files via upload

---
+++ src/main/java/com/doctor/SmartCatShelterApplication.java
@@ -0,0 +1,11 @@
+package com.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCatShelterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCatShelterApplication.class, args);
+    }
+}
